12eaf72d7a95f8c4b082289f69fdb67e582a3c19
Suhan <suhanaksu13@gmail.com>
2024-08-14 23:58:46 +0300

    routing done

---
+++ src/components/auth/home-check-layout.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import RouterLink from 'next/link';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import { paths } from '@/paths';
+import { DynamicLogo } from '@/components/core/logo';
+
+export interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export function HomeCheckLayout({ children }: LayoutProps): React.JSX.Element {
+    return (
+        <Box
+            sx={{
+                display: { xs: 'flex', lg: 'flex' },
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'row',
+                gridTemplateColumns: '1fr 1fr',
+                minHeight: '100%',
+            }}
+        >
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
+                    <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
+                </Box>
+            </Box>
+
+        </Box>
+    );
+}
